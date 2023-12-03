b6c47d3a98c1f75d3e36bfc1b6d644880c51584d
Kévin Tollemer <kevin.tollemer@insa-rennes.fr>
2023-12-03 19:51:29 +0100

    2015 day 9 ongoing
    -Puzzle 1 still not finished

--- src/Year2015/day9/puzzle1/Path.java
+++ src/Year2015/day9/puzzle1/Path.java
@@ -2,6 +2,7 @@ package Year2015.day9.puzzle1;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -9,20 +10,26 @@ import java.util.Scanner;
 public class Path {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("src/Year2015/day9/test.txt");
+        File file = new File("src/Year2015/day9/input.txt");
         Scanner scanner = new Scanner(file);
-        HashMap<String[], Integer> distances = new HashMap<>();
+        HashMap<String[], Integer> roads = new HashMap<>();
+        ArrayList<Integer> distances = new ArrayList<>();
+        ArrayList<String> cities = new ArrayList<>();
         while (scanner.hasNext()) {
             String line = scanner.nextLine();
-            distances.put(new String[]{line.split(" ")[0], line.split(" ")[2]}, Integer.parseInt(line.split(" ")[4]));
+            roads.put(new String[]{line.split(" ")[0], line.split(" ")[2]}, Integer.parseInt(line.split(" ")[4]));
         }
-
+        for (String[] city : roads.keySet()) {
+            if (!cities.contains(city[0])) cities.add(city[0]);
+            if (!cities.contains(city[1])) cities.add(city[1]);
+        }
         /**
          * OUTPUT
          */
-        for (String[] array:distances.keySet()) {
-            System.out.println(Arrays.toString(array) + " : " + distances.get(array));
+        for (String[] array:roads.keySet()) {
+            System.out.println(Arrays.toString(array) + " : " + roads.get(array));
         }
+        System.out.println("Nb de villes : " + cities.size());
     }
 
 }
