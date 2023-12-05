f4ffede00fa2b0937ca63d2791abf7c37d05d5a2
joshuadwightred <tollemerkevin@gmail.com>
2023-12-05 13:26:21 +0100

    2023 day 5 ongoing
    -Puzzle 1 finished & 2 started !

--- src/Year2023/day5/puzzle2/SeedImproved.java
+++ src/Year2023/day5/puzzle2/SeedImproved.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 public class SeedImproved {
 
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("src/Year2023/day5/test.txt");
+        File file = new File("src/Year2023/day5/input.txt");
         Scanner scanner = new Scanner(file);
         String line = scanner.nextLine();
         long[] seeds = Arrays.stream(line.split(":")[1].strip().split(" ")).mapToLong(Long::parseLong).toArray();
