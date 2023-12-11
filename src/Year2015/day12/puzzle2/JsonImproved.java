506def4a994a5c5dcb16bb18e603c52dcae63ac4
Kévin Tollemer <kevin.tollemer@insa-rennes.fr>
2023-12-11 14:29:40 +0100

    Day 12 Year 2015 ongoing
    - Puzzle 2 ongoing

--- src/Year2015/day12/puzzle2/JsonImproved.java
+++ src/Year2015/day12/puzzle2/JsonImproved.java
@@ -1,9 +1,36 @@
 package Year2015.day12.puzzle2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class JsonImproved {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/Year2015/day12/test.txt");
+        Scanner scanner = new Scanner(file);
+        StringBuilder line = new StringBuilder(scanner.nextLine());
+        ArrayList<String> sections = new ArrayList<>();
+        for (int i = 0; i < line.length()-1; i++) {
+            if (line.charAt(i) == '{' || line.charAt(i) == '[' || !(line.charAt(i+1) == '{' || line.charAt(i+1) == '[')) {
+                int j;
+                int breakingPoint = 1;
+                for (j = i+1; j < line.length(); j++) {
+                    if (line.charAt(j) == '{' || line.charAt(j) == '[') breakingPoint++;
+                    if (line.charAt(j) == '}' || line.charAt(j) == ']') breakingPoint--;
+                    if (breakingPoint == 0) break;
+                }
+                sections.add(line.substring(i, j+1));
+                line.replace(i, j+2, "");
+                i = 0;
+            }
+            if (!line.toString().contains("{") || !line.toString().contains("}")) break;
+        }
+        sections.add(line.toString());
+        for (String s : sections) {
+            System.out.println(s);
+        }
     }
 
 }
