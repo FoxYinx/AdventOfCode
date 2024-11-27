5e8822ef6d1f6e75b6be50baf9ee5df816497a71
yinx <tollemerkevin@gmail.com>
2024-11-27 15:39:01 +0100

    Year 2022 - Day 2 - Puzzle 1

--- src/Year2022/day2/puzzle1/RPS.java
+++ src/Year2022/day2/puzzle1/RPS.java
@@ -13,9 +13,27 @@ public class RPS {
         while (scanner.hasNext()) {
             line = scanner.nextLine();
             switch (line.charAt(0)) {
-                case 'A' -> score += 8;
-                case 'B' -> score++;
-                case 'C' -> score += 6;
+                case 'A' -> {
+                    switch (line.charAt(2)) {
+                        case 'X' -> score += 4;
+                        case 'Y' -> score += 8;
+                        case 'Z' -> score += 3;
+                    }
+                }
+                case 'B' -> {
+                    switch (line.charAt(2)) {
+                        case 'X' -> score += 1;
+                        case 'Y' -> score += 5;
+                        case 'Z' -> score += 9;
+                    }
+                }
+                case 'C' -> {
+                    switch (line.charAt(2)) {
+                        case 'X' -> score += 7;
+                        case 'Y' -> score += 2;
+                        case 'Z' -> score += 6;
+                    }
+                }
             }
         }
         System.out.println("My total score is " + score);
